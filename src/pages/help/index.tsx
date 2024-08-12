038a3c295086c0eb0038d0b58987e9afa70de1cf
mk <kamai122000@mail.ru>
2024-08-12 12:54:37 +0300

    data

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -10,34 +10,33 @@ import { Label } from 'shared/ui/label'
 import { Button } from 'shared'
 
 import useWindowSize from 'react-use/lib/useWindowSize'
-import OpenAI from 'openai'
 
-const data = [
-	{
-		title: 'Строительство мечети в с.Барда Пермского края',
-		description: 'Строительство мечети в с.Барда Пермского края',
-		image: '/help/3.jpeg',
-		money: '452 000',
-		need: '800 000',
-	},
-	{
-		title: 'Барашка',
-		description: '',
-		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/fd29933b-c39c-4706-9f64-d87f999eb7b4/Default_a_lamb_in_a_cartoon_style_3.jpg',
-		money: '20 000',
-	}, {
-		title: 'Корова',
-		description: '',
-		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/7f24e7ec-bded-48bf-b1b8-a8a04f4babbe/Default_a_cow_in_cartoon_style_1.jpg',
-		money: '80 000',
-	},
-	{
-		title: 'Верблюд',
-		description: '',
-		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/bc4937ad-5691-4ff8-974f-5c507fe652f1/Default_a_camel_in_cartoon_style_1.jpg',
-		money: '135 000',
-	},
-]
+// const data = [
+// 	{
+// 		title: 'Строительство мечети в с.Барда Пермского края',
+// 		description: 'Строительство мечети в с.Барда Пермского края',
+// 		image: '/help/3.jpeg',
+// 		money: '452 000',
+// 		need: '800 000',
+// 	},
+// 	{
+// 		title: 'Барашка',
+// 		description: '',
+// 		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/fd29933b-c39c-4706-9f64-d87f999eb7b4/Default_a_lamb_in_a_cartoon_style_3.jpg',
+// 		money: '20 000',
+// 	}, {
+// 		title: 'Корова',
+// 		description: '',
+// 		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/7f24e7ec-bded-48bf-b1b8-a8a04f4babbe/Default_a_cow_in_cartoon_style_1.jpg',
+// 		money: '80 000',
+// 	},
+// 	{
+// 		title: 'Верблюд',
+// 		description: '',
+// 		image: 'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/bc4937ad-5691-4ff8-974f-5c507fe652f1/Default_a_camel_in_cartoon_style_1.jpg',
+// 		money: '135 000',
+// 	},
+// ]
 
 const dataImage = [
 	'https://cdn.leonardo.ai/users/7eed60b5-bb28-4022-9a1e-739aa1ca9674/generations/fbf9be2c-b783-4cfa-b49a-2be3016f8c0e/Default_Create_an_original_image_of_a_mosque_icon_in_a_network_0.jpg',
@@ -57,21 +56,6 @@ export const Help = () => {
 	const [numberPeace, setNumberPeace] = useState(0)
 	const [image, setImage] = useState('')
 	const [type, setType] = useState('')
-
-	const client = new OpenAI({
-		apiKey: import.meta.env.VITE_OPENAI, // This is the default and can be omitted
-		dangerouslyAllowBrowser: true,
-	})
-
-	async function main() {
-		const params: OpenAI.Images.ImageGenerateParams = {
-			prompt: 'Generate an original image-a lamb badge in the nft style. The whole lamb should be displayed in the picture, painted in an original way. The surface of the lamb can be covered with an interesting original ornament. A lamb should be sweet and sincere.',
-			n: 1,  // Количество изображений, которые нужно сгенерировать
-			size: '1024x1024',  // Размер изображения: 256x256, 512x512, 1024x1024
-		}
-		const chatCompletion = await client.images.generate(params)
-		setImage(chatCompletion.data[0].url!)
-	}
 
 	const genImage = () => {
 		console.log(type)
@@ -109,11 +93,11 @@ export const Help = () => {
 
 		<Dialog>
 			<div className={'row-2 flex-wrap '}>
-				{
-					data.map((value) => <Card {...value} key={value.title} set={(value) => {
-						setType(value)
-					}}/>)
-				}
+				{/*{*/}
+				{/*	data.map((value) => <Card {...value} key={value.title} set={(value) => {*/}
+				{/*		setType(value)*/}
+				{/*	}}/>)*/}
+				{/*}*/}
 			</div>
 			<DialogContent className={'sm:max-w-[425px]'}>
 				<DialogHeader>
